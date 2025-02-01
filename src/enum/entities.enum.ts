9c0494cb6ac4490f107b5324eff8b8b18f77ec7e
MarianJax <119023432+MarianJax@users.noreply.github.com>
2025-02-01 04:24:14 +0000

    Agregar enums para Jornada, DiaSemana y RolHorario en entities.enum.ts

---
+++ src/enum/entities.enum.ts
@@ -0,0 +1,20 @@
+
+export enum Jornada {
+    VESPERTINA = 'Vespertina',
+    MATUTINA = 'Matutina',
+    NOCTURNA = 'Nocturna',
+}
+
+export enum DiaSemana {
+    LUNES = 'Lunes',
+    MARTES = 'Martes',
+    MIERCOLES = 'Miercoles',
+    JUEVES = 'Jueves',
+    VIERNES = 'Viernes',
+    SABADO = 'Sabado',
+    DOMINGO = 'Domingo',
+}
+export enum RolHorario {
+    ADMINISTRADOR = 'Administrador',
+    ENTRENADOR = 'Entrenador',
+}
