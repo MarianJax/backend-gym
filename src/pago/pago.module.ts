6388258e0cb98a86d2bb7e11fb267416daf7aea0
MarianJax <mparrales2018@utm.edu.ec>
2025-02-28 18:27:44 -0500

    Refactor Pago module to remove Membresia dependency and enhance service exports

--- src/pago/pago.module.ts
+++ src/pago/pago.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Membresia } from 'src/membresia/entities/membresia.entity';
 import { Pago } from './entities/pago.entity';
 import { PagoController } from './pago.controller';
 import { PagoService } from './pago.service';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Pago, Membresia])],
+  imports: [TypeOrmModule.forFeature([Pago])],
   controllers: [PagoController],
   providers: [PagoService],
-  exports: [TypeOrmModule],
+  exports: [PagoService],
 })
 export class PagoModule { }
