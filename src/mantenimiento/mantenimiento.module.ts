4419d7f7d4cbdb20e1b180d9481c81784d39648f
MarianJax <mparrales2018@utm.edu.ec>
2025-02-28 18:27:30 -0500

    Refactor Mantenimiento module and service to improve service accessibility and utilize MaquinaService for machine operations

--- src/mantenimiento/mantenimiento.module.ts
+++ src/mantenimiento/mantenimiento.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MaquinaModule } from 'src/maquina/maquina.module';
-import { MaquinaService } from 'src/maquina/maquina.service';
 import { Mantenimiento } from './entities/mantenimiento.entity';
 import { MantenimientoController } from './mantenimiento.controller';
 import { MantenimientoService } from './mantenimiento.service';
+
 @Module({
   imports: [TypeOrmModule.forFeature([Mantenimiento]), MaquinaModule],
   controllers: [MantenimientoController],
-  providers: [MantenimientoService, MaquinaService],
-  exports: [TypeOrmModule],
+  providers: [MantenimientoService],
+  exports: [MantenimientoService],
 
 })
 export class MantenimientoModule { }
--- src/mantenimiento/mantenimiento.service.ts
+++ src/mantenimiento/mantenimiento.service.ts
@@ -1,34 +1,34 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Estado, Maquina } from 'src/maquina/entities/maquina.entity';
 import { Repository } from 'typeorm';
 import { CreateMantenimientoDto } from './dto/create-mantenimiento.dto';
 import { UpdateMantenimientoDto } from './dto/update-mantenimiento.dto';
 import { Mantenimiento, Estado as EstMant } from './entities/mantenimiento.entity';
+import { MaquinaService } from '../maquina/maquina.service';
+import { Estado } from '../enum/entities.enum';
 
 @Injectable()
 export class MantenimientoService {
   constructor(
     @InjectRepository(Mantenimiento)
-    private readonly MantenimientoRepository: Repository<Mantenimiento>,
+    private readonly mantenimientoRepository: Repository<Mantenimiento>,
 
-    @InjectRepository(Maquina)
-    private readonly maquinaRepository: Repository<Maquina>,
+    private readonly maquinaService: MaquinaService,
   ) { }
 
   async create(createMantenimientoDto: CreateMantenimientoDto): Promise<Mantenimiento | null> {
     try {
-      const maquina = await this.maquinaRepository.findOneByOrFail({ id: createMantenimientoDto.maquina_id });
+      const maquina = await this.maquinaService.findOne(createMantenimientoDto.maquina_id);
 
       maquina.estado = createMantenimientoDto.estado === EstMant.FINALIZADO ? Estado.DISPONIBLE : Estado.MANTENIMIENTO;
-      await this.maquinaRepository.save(maquina);
+      await this.maquinaService.update(maquina.id, maquina);
 
-      const mantenimiento = this.MantenimientoRepository.create({
+      const mantenimiento = this.mantenimientoRepository.create({
         ...createMantenimientoDto,
         maquina,
       });
 
-      return await this.MantenimientoRepository.save(mantenimiento);
+      return await this.mantenimientoRepository.save(mantenimiento);
     } catch (error) {
       console.error('La maquina no existe', error);
       if (error.code === '22P02' && error.routine === 'string_to_uuid') {
@@ -38,18 +38,18 @@ export class MantenimientoService {
   }
 
   async findAll(): Promise<Mantenimiento[]> {
-    return await this.MantenimientoRepository.find();
+    return await this.mantenimientoRepository.find();
   }
 
   async findOne(id: string): Promise<Mantenimiento> {
-    return await this.MantenimientoRepository.findOneBy({ id });
+    return await this.mantenimientoRepository.findOneBy({ id });
   }
 
   async update(id: string, updateMantenimientoDto: UpdateMantenimientoDto): Promise<void> {
-    await this.MantenimientoRepository.update(id, updateMantenimientoDto);
+    await this.mantenimientoRepository.update(id, updateMantenimientoDto);
   }
 
   async remove(id: string): Promise<void> {
-    await this.MantenimientoRepository.delete(id);
+    await this.mantenimientoRepository.delete(id);
   }
 }
