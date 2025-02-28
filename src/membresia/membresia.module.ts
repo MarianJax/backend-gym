19cc382c2efcf4ecf11c3136d3563d3c86f93532
MarianJax <mparrales2018@utm.edu.ec>
2025-02-28 18:27:38 -0500

    Refactor Membresia module and service to enhance service accessibility by removing direct repository injections and utilizing UserService and PagoService for user and payment operations

--- src/membresia/membresia.module.ts
+++ src/membresia/membresia.module.ts
@@ -4,14 +4,13 @@ import { UserModule } from 'src/user/user.module';
 import { Membresia } from './entities/membresia.entity';
 import { MembresiaController } from './membresia.controller';
 import { MembresiaService } from './membresia.service';
-import { UserService } from 'src/user/user.service';
 import { PagoModule } from 'src/pago/pago.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Membresia]), UserModule, PagoModule],
   controllers: [MembresiaController],
-  providers: [MembresiaService, UserService, PagoModule],
-  exports: [TypeOrmModule],
+  providers: [MembresiaService],
+  exports: [MembresiaService],
 
 })
 export class MembresiaModule { }
--- src/membresia/membresia.service.ts
+++ src/membresia/membresia.service.ts
@@ -1,29 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
-import { Repository } from 'typeorm';
+import { LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
 import { CreateMembresiaDto } from './dto/create-membresia.dto';
 import { UpdateMembresiaDto } from './dto/update-membresia.dto';
 import { Membresia } from './entities/membresia.entity';
-import { Pago } from 'src/pago/entities/pago.entity';
+import { UserService } from 'src/user/user.service';
+import { PagoService } from 'src/pago/pago.service';
 
 @Injectable()
 export class MembresiaService {
   constructor(
     @InjectRepository(Membresia)
     private readonly MembresiaRepository: Repository<Membresia>,
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-    @InjectRepository(Pago)
-    private pagoRepository: Repository<Pago>,
+
+    private readonly userService: UserService,
+    private readonly pagoService: PagoService,
   ) { }
 
   async create(
     createMembresiaDto: CreateMembresiaDto,
   ): Promise<Membresia> {
     try {
-      const users = await this.findUser(createMembresiaDto.usuario_id);
-      const pagos = await this.findPago(createMembresiaDto.pago_id);
+      const users = await this.userService.findOne(createMembresiaDto.usuario_id);
+      const pagos = await this.pagoService.findOne(createMembresiaDto.pago_id);
 
       const membresia = this.MembresiaRepository.create({
         ...createMembresiaDto,
@@ -36,36 +35,6 @@ export class MembresiaService {
       console.log(error);
       if (error.code === '23505') {
         throw new Error('El usuario ya tiene una membresía activa');
-      }
-    }
-  }
-
-  async findUser(id: string): Promise<User> {
-    try {
-      return await this.userRepository.findOneOrFail({
-        where: {
-          id: id
-        }
-      });
-    } catch (error) {
-      console.log(error);
-      if (error.code === '22P02' && error.routine === 'string_to_uuid') {
-        throw new Error('El usuario ya tiene una membresía activa');
-      }
-    }
-  }
-
-  async findPago(id: string): Promise<Pago> { 
-    try {
-      return await this.pagoRepository.findOneOrFail({
-        where: {
-          id: id
-        }
-      });
-    } catch (error) {
-      console.log(error);
-      if (error.code === '22P02' && error.routine === 'string_to_uuid') {
-        throw new Error('El pago no existe');
       }
     }
   }
@@ -95,4 +64,22 @@ export class MembresiaService {
   async remove(id: string): Promise<void> {
     await this.MembresiaRepository.delete(id);
   }
+
+  async findActiveMembresiaByIdAndDate(id: string, fecha: Date): Promise<Membresia> {
+    try {
+      return await this.MembresiaRepository.findOneOrFail({
+        where: {
+          id,
+          fecha_fin: MoreThanOrEqual(fecha),
+          fecha_inicio: LessThanOrEqual(fecha),
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException(
+        'No posee una membresía activa para agendar',
+      );
+    }
+  }
+  
 }
