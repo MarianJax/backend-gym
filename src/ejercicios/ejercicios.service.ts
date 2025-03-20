e8601b8fb1623f9d620d41ee4e39d575bfc2aafa
MarianJax <mparrales2018@utm.edu.ec>
2025-03-19 19:05:37 -0500

    Refactor EjerciciosService to improve error handling, update logic, and enhance data retrieval with relations

--- src/ejercicios/ejercicios.service.ts
+++ src/ejercicios/ejercicios.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { CreateEjercicioDto } from './dto/create-ejercicio.dto';
-import { UpdateEjercicioDto } from './dto/update-ejercicio.dto';
-import { RutinaService } from 'src/rutina/rutina.service';
-import { Repository } from 'typeorm';
-import { Ejercicio } from './entities/ejercicio.entity';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MaquinaService } from 'src/maquina/maquina.service';
+import { RutinaService } from 'src/rutina/rutina.service';
+import { Repository } from 'typeorm';
+import { CreateEjercicioDto } from './dto/create-ejercicio.dto';
+import { UpdateEjercicioDto } from './dto/update-ejercicio.dto';
+import { Ejercicio } from './entities/ejercicio.entity';
 
 @Injectable()
 export class EjerciciosService {
@@ -29,27 +29,39 @@ export class EjerciciosService {
       maquinas,
     });
 
+    rutinas.forEach(rutina => {
+      rutina.cantidad_ejercicios += 1;
+    });
+
+    await this.rutinaService.updatedCantidadEjercicios(rutinas);
+
     await this.ejercicioRepository.save(ejercicio);
 
     return ejercicio;
   }
   async findAll(): Promise<Ejercicio[]> {
-    return await this.ejercicioRepository.find();
+    return await this.ejercicioRepository.find({ relations: ['rutinas', 'maquinas'] });
   }
 
   async findOne(id: string): Promise<Ejercicio> {
-    return await this.ejercicioRepository.findOneBy({ id });
+    return await this.ejercicioRepository.findOne({ where: { id }, relations: ['rutinas', 'maquinas'] });
   }
 
-  async update(id: string, updateEjercicioDto: UpdateEjercicioDto): Promise<void> {
-    const rutinas = await this.rutinaService.findAllById(updateEjercicioDto.rutinas);
-    const maquinas = await this.maquinaService.findAllById(updateEjercicioDto.maquinas);
+  async update(id: string, { maquinas: mqs, rutinas: rts, ...data }: UpdateEjercicioDto): Promise<void> {
+    const rutinas = await this.rutinaService.findAllById(rts);
+    const maquinas = await this.maquinaService.findAllById(mqs);
 
-    await this.ejercicioRepository.update(id, {
-      ...updateEjercicioDto,
-      rutinas,
-      maquinas,
-    });
+    const ejercicio = await this.ejercicioRepository.findOne({ where: { id } });
+
+    if (!ejercicio) {
+      throw new BadRequestException('Ejercicio no encontrado');
+    }
+
+    Object.assign(ejercicio, data);
+    ejercicio.rutinas = rutinas;
+    ejercicio.maquinas = maquinas;
+
+    await this.ejercicioRepository.save(ejercicio);
   }
 
   async remove(id: string): Promise<void> {
