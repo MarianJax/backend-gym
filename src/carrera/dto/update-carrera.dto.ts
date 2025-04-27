76f2ae3c6fcf603e8012afe0ae3c646595c75c0b
MarianJax <mparrales2017@utm.edu.ec>
2025-04-26 23:55:08 -0500

    Eliminar módulos, controladores, servicios y DTOs de Carrera y Facultad para simplificar la estructura del proyecto.

--- src/carrera/dto/update-carrera.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCarreraDto } from './create-carrera.dto';
-
-export class UpdateCarreraDto extends PartialType(CreateCarreraDto) {}
--- src/facultad/dto/update-facultad.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFacultadDto } from './create-facultad.dto';
-
-export class UpdateFacultadDto extends PartialType(CreateFacultadDto) {}
