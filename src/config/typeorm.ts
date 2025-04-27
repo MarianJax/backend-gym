d9d60884909cf4474715999b057efa3c7453a4c8
MarianJax <mparrales2017@utm.edu.ec>
2025-04-27 04:07:38 -0500

    Eliminar módulo de usuario y sus componentes para simplificar la estructura del proyecto

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,16 +1,13 @@
 import { Ejercicio } from '../ejercicios/entities/ejercicio.entity';
 import { Agendamiento } from '../agendamiento/entities/agendamiento.entity';
-import { Carrera } from '../carrera/entities/carrera.entity';
-import { Facultad } from '../facultad/entities/facultad.entity';
 import { Horario } from '../horario/entities/horario.entity';
 import { HorarioEmpleado } from '../horario_empleado/entities/horario_empleado.entity';
 import { Mantenimiento } from '../mantenimiento/entities/mantenimiento.entity';
 import { Maquina } from '../maquina/entities/maquina.entity';
 import { Membresia } from '../membresia/entities/membresia.entity';
 import { Pago } from '../pago/entities/pago.entity';
-import { Rol } from '../rol/entities/rol.entity';
+import { Distribucion } from '../distribucion/entities/distribucion.entity';
 import { Rutina } from '../rutina/entities/rutina.entity';
-import { User } from '../user/entities/user.entity';
 import { ValidacionesPago } from '../validaciones_pago/entities/validaciones_pago.entity';
 import { DataSource } from 'typeorm';
 
@@ -23,7 +20,19 @@ const AppDataSource = new DataSource({
   database: 'gymutm',
   schema: 'esq_gimnasio',
   synchronize: false,
-  entities: [Agendamiento, Carrera,User, Facultad, Horario, HorarioEmpleado, Mantenimiento, Maquina, Membresia, Pago, Rol, Rutina, ValidacionesPago, Ejercicio ],
+  entities: [
+    Agendamiento,
+    Horario,
+    HorarioEmpleado,
+    Mantenimiento,
+    Maquina,
+    Membresia,
+    Pago,
+    Distribucion,
+    Rutina,
+    ValidacionesPago,
+    Ejercicio,
+  ],
   migrations: ['./src/database/*-migrations.ts'],
   migrationsRun: true,
   logging: true,
